bbbfa4b1556a8e5ea3c79d653960755b4773b767
David-Happel <davidvhappel1@gmail.com>
2021-03-31 15:07:14 +0200

    query transcripts

--- search.py
+++ search.py
@@ -4,7 +4,7 @@ import logging, json
 import pprint
 from utils import find_time
 
-search_word = "porn"
+search_word = "you"
 
 def search(es, index_name, search):
     res = es.search(index=index_name, body=search)
@@ -22,32 +22,32 @@ def connect_elasticsearch():
 if __name__ == '__main__':
     es = connect_elasticsearch()
     if es is not None:
-        search_object = {'query': { 
+        search_object = { "from" : 0,
+        "size" : 50,
+        # 'explain': True,
+        'query': { 
            "nested": {
                 "path": "clips",
                 "query": {
-                    "nested": {
-                        "path": "clips.words",
-                        "query": {
-                            "bool": {
-                                "should": [
-                                    {
-                                    "match_phrase": {
-                                        "clips.words.word": search_word
-                                    }
-                                    }
-                                ],
-                                "minimum_should_match": 1
+                    "bool": {
+                        "should": [
+                            {
+                            "match_phrase": {
+                                "clips.transcript": search_word
                             }
-                        },
-                        "score_mode": "avg"
-                        }
+                            }
+                        ],
+                        "minimum_should_match": 1
+                    }
                 },
                 "score_mode": "avg"
                 }
             }}
         res = search(es, 'podcasts', search_object)
+        # pp = pprint.PrettyPrinter(indent=4)
+        # pp.pprint(res["explanation"])
         for hit in res['hits']['hits']:
-            for item in hit["_source"]["clips"]:
-                if search_word in item["transcript"]:
-                    find_time(search_word, item)
+            print(hit["_score"])
+            # for item in hit["_source"]["clips"]:
+            #     if search_word in item["transcript"]:
+            #         find_time(search_word, item)
